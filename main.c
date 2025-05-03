ba8acae865c3cf7c39837c33e510e6fc6e78562a
arhitector <arhitector666@gmail.com>
2025-05-03 13:56:19 +0200

    remove comments

--- main.c
+++ main.c
@@ -1,39 +1,37 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define REED_PIN PB2   // Reed switch input pin (P2)
-#define BUTTON_PIN PB1 // Pin to simulate button press (P1)
+#define REED_PIN PB2
+#define BUTTON_PIN PB1
 
 void setup() {
-    // Configure REED_PIN as input with internal pull-up
-    DDRB &= ~(1 << REED_PIN);  // Set REED_PIN as input
-    PORTB |= (1 << REED_PIN);  // Enable internal pull-up resistor
+    DDRB &= ~(1 << REED_PIN);
+    PORTB |= (1 << REED_PIN);
 
-    // Configure BUTTON_PIN as output
-    DDRB |= (1 << BUTTON_PIN); // Set PB1 as output
-    PORTB &= ~(1 << BUTTON_PIN); // Default PB1 LOW
+    DDRB |= (1 << BUTTON_PIN);
+    PORTB &= ~(1 << BUTTON_PIN);
 }
 
 void pressButton() {
-    PORTB |= (1 << BUTTON_PIN);  // Set PB1 HIGH (simulate button press)
-    _delay_ms(50);               // Short press duration
-    PORTB &= ~(1 << BUTTON_PIN); // Return PB1 to LOW
+    PORTB |= (1 << BUTTON_PIN);
+    _delay_ms(50);
+    PORTB &= ~(1 << BUTTON_PIN);
 }
 
 int main(void) {
     setup();
 
-    uint8_t lastState = 1; // Start with reed switch default state (HIGH)
+    uint8_t lastState = 1;
 
     while (1) {
-        uint8_t currentState = PINB & (1 << REED_PIN); // Read REED_PIN state
+        uint8_t currentState = PINB & (1 << REED_PIN);
 
-        if (currentState == 0 && lastState != 0) { // Detect transition from HIGH to LOW
-            pressButton(); // Simulate button press
+        if (currentState == 0 && lastState != 0) {
+            pressButton();
         }
 
-        lastState = currentState; // Update last state
-        _delay_ms(20000);         // Check every 20 seconds
+        lastState = currentState;
+        _delay_ms(500);
     }
 
     return 0;
